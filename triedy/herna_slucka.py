79a4edf80c4301e1c1d2a50bf7182a2b26f2f0d5
Kevin Svitač <kevin@svit.ac>
2024-12-24 20:15:37 +0100

    fungujúce svetlo

--- triedy/herna_slucka.py
+++ triedy/herna_slucka.py
@@ -5,7 +5,7 @@ Modul pre hernú slučku.
 from pathlib import Path
 import pygame
 
-from triedy.sprite import OsvetlenySprite
+from triedy.sprite import Sprite, OsvetlenySprite
 
 
 class HernaSlucka(pygame.sprite.Group):
@@ -33,7 +33,7 @@ class HernaSlucka(pygame.sprite.Group):
         )
 
         self.add(
-            OsvetlenySprite(
+            Sprite(
                 pozicia=(0, 0),
                 velkost=(16 * 5, 16 * 5),
                 cesta_k_obrazku=self.cesta_k_assetom / "test.png",
@@ -49,7 +49,7 @@ class HernaSlucka(pygame.sprite.Group):
 
             self.screen.fill("black")
 
-            test_sprite.uroven_svetla -= 0.1
+            test_sprite.uroven_svetla -= 0.01
             if test_sprite.uroven_svetla <= 0.0:
                 test_sprite.uroven_svetla = 1.0
 
--- triedy/sprite/osvetleny_sprite.py
+++ triedy/sprite/osvetleny_sprite.py
@@ -17,16 +17,15 @@ class OsvetlenySprite(Sprite):
     ):
         super().__init__(pozicia, velkost, cesta_k_obrazku)
         self.uroven_svetla = 1.0
-
-        self.svetlo = pygame.Surface(velkost, pygame.SRCALPHA)
-        self.svetlo.fill((0, 0, 0, 0))  # čierna farba (predstavuje "tmu")
-
-        self._vykresli_svetlo()
+        self.originalny_obrazok: pygame.Surface = (
+            self.image.copy()
+        )  # pred aplikovaním svetla
 
     def _vykresli_svetlo(self):
-        self.svetlo.set_alpha(round(255 * self.uroven_svetla))  # nastaví priehľadnosť
-        self.image.blit(self.svetlo, (0, 0))  # vloží svetlo na vrch pôvodného obrázku
+        alpha = round(255 * (1 - self.uroven_svetla))
+
+        self.image = self.originalny_obrazok.copy()
+        self.image.fill((0, 0, 0, alpha), special_flags=pygame.BLEND_RGBA_SUB)
 
     def update(self):
         self._vykresli_svetlo()
-        super().update()
