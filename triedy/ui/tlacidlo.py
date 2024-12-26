e5ec7515879d548c61055bd374314361e0e37396
Kevin Svitač <kevin@svit.ac>
2024-12-26 12:03:12 +0100

    ešte lepšie tlačidlá

--- triedy/ui/tlacidlo.py
+++ triedy/ui/tlacidlo.py
@@ -9,18 +9,24 @@ from triedy.sprite import Sprite
 class Tlacidlo(Sprite):
     def __init__(self, pozicia: tuple[int, int], text: str, po_kliknuti: t.Callable):
         super().__init__(pozicia, (300, 150), Path("ui") / "tlacidlo.png")
-        self.text = text
         self.po_kliknuti = po_kliknuti
+        self.velkost_textu = self.velkost[1] // 4
 
         self.kliknute = False
         self.obrazok_original = self.image.copy()
         self.obrazok_kliknute = self.nacitaj_obrazok(Path("ui") / "tlacidlo_klik.png")
         self.obrazok_hover = self.nacitaj_obrazok(Path("ui") / "tlacidlo_hover.png")
+        self.text = pygame.font.Font(self.ASSETY_ROOT / "FiraCode.ttf", self.velkost_textu).render(text, True, (0, 0, 0))
 
     def update(self):
         super().update()
+
+        # na tlačidlo ešte vypíšeme text tlačidla:
+        img_rect = self.image.get_rect()
         self.image.blit(
-            pygame.font.Font(None, 50).render(self.text, True, (0, 0, 0)), (10, 10)
+            self.text,
+            # približne na stred - je ťažké vypočítať presný stred, pretože rôzne fonty majú rôznu šírku:
+            (img_rect.centerx - self.velkost_textu, img_rect.centery - (self.velkost_textu // 2)),
         )
 
     def spracuj_event(self, event: pygame.event.Event):
@@ -28,18 +34,19 @@ class Tlacidlo(Sprite):
             if self.rect.collidepoint(event.pos):
                 self.kliknute = True
                 self.image = self.obrazok_kliknute
-        elif event.type == pygame.MOUSEBUTTONUP:  # klikneme a pustíme myš
-            if self.kliknute:
-                self.kliknute = False
-                self.image = self.obrazok_original
+        elif event.type == pygame.MOUSEBUTTONUP and self.kliknute:  # pustíme myš
+            self.kliknute = False
+            self.image = self.obrazok_original
 
-                if self.rect.collidepoint(event.pos):  # ak sme si to nerozmysleli
-                    self.po_kliknuti()
-        elif event.type == pygame.MOUSEMOTION:
+            if self.rect.collidepoint(event.pos):  # ak sme si to nerozmysleli
+                self.po_kliknuti()
+        elif event.type == pygame.MOUSEMOTION:  # pohyb myši
             if self.kliknute:
                 return
 
-            if self.rect.collidepoint(event.pos):
+            if self.rect.collidepoint(event.pos):  # pohybujeme sa v rámci tlačidla
                 self.image = self.obrazok_hover
             else:
+                # tuto je nevýhoda že sa to nastavuje za každým pohybom myši mimo tlačidla
+                # ...ale nie je čas vymýšľať sofistikovanejšiu logiku:
                 self.image = self.obrazok_original
